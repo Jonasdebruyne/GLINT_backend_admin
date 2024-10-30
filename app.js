6b62d86af44c1f538dbdb0123d6d5a567dcd080a
JonasDeBruyne <jonasdebruyne734@gmail.com>
2024-10-30 15:25:44 +0100

    fixed

--- app.js
+++ app.js
@@ -5,47 +5,51 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const config = require("config");
 const cors = require("cors");
-const connection = config.get("mongodb");
-const passport = require("./passport/passport");
 const mongoose = require("mongoose");
-mongoose.connect(connection);
+
+// Database connectie
+const connection = config.get("mongodb");
+mongoose
+  .connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Routers
 const productRouter = require("./routes/api/v1/products");
 const userRouter = require("./routes/api/v1/users");
 const houseStyleRouter = require("./routes/api/v1/houseStyle");
+
+// Express-app
 const app = express();
 
-// Set the port
-const PORT = process.env.PORT || 3000; // Gebruik poort uit omgevingsvariabelen of 3000 als standaard
-
+// View engine instellen
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+// Middleware
 app.use(cors());
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// API-routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/houseStyle", houseStyleRouter);
 
+// Foutafhandeling voor 404
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
+// Algemene foutafhandeling
 app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
   res.render("error");
-});
-
-// Start de server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
