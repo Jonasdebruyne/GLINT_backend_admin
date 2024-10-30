e7cf122e5d1b5eeb1c1b838b4144bd847a8bdae5
JonasDeBruyne <jonasdebruyne734@gmail.com>
2024-10-30 14:10:35 +0100

    feat: app.js updated

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const userRouter = require("./routes/api/v1/users");
 const houseStyleRouter = require("./routes/api/v1/housestyle");
 const app = express();
 
+// Set the port
+const PORT = process.env.PORT || 3000; // Gebruik poort uit omgevingsvariabelen of 3000 als standaard
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
@@ -26,7 +29,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1/users", userRouter);
-
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/housestyle", houseStyleRouter);
 
@@ -39,6 +41,11 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
   res.render("error");
+});
+
+// Start de server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
