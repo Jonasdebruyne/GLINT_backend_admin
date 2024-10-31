c704d979cf1416c970d931dbe5416d4cde3e85a1
JonasDeBruyne <jonasdebruyne734@gmail.com>
2024-10-31 20:11:34 +0100

    feat: added option owner by role in userController

--- models/api/v1/User.js
+++ models/api/v1/User.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ["user", "admin"],
+    enum: ["user", "admin", "owner"],
     default: "user",
   },
   activeUnactive: {
